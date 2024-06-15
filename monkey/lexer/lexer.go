4489cf77d551244bfe9e466eae6bb29cc7091467
Hiro <leo@yamamotoyuutas-MacBook-Pro.local>
2024-06-15 15:58:11 +0900

    chapter 1

--- monkey/lexer/lexer.go
+++ monkey/lexer/lexer.go
@@ -43,7 +43,33 @@ func (l *Lexer) NextToken() token.Token {
 	fmt.Println("l.ch: ", l.ch)
 	switch l.ch {
 	case '=':
-		tok = newToken(token.ASSIGN, l.ch)
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			litral := string(ch) + string(l.ch)
+			tok = token.Token{Type: token.EQ, Literal: litral}
+		} else {
+			tok = newToken(token.ASSIGN, l.ch)
+		}
+	case '-':
+		tok = newToken(token.MINUS, l.ch)
+	case '!':
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			litral := string(ch) + string(l.ch)
+			tok = token.Token{Type: token.NOT_EQ, Literal: litral}
+		} else {
+			tok = newToken(token.BANG, l.ch)
+		}
+	case '/':
+		tok = newToken(token.SLASH, l.ch)
+	case '*':
+		tok = newToken(token.ASTERISK, l.ch)
+	case '<':
+		tok = newToken(token.LT, l.ch)
+	case '>':
+		tok = newToken(token.GT, l.ch)
 	case ';':
 		tok = newToken(token.SEMICOLON, l.ch)
 	case '(':
@@ -113,4 +139,12 @@ func (l *Lexer) readNumber() string {
 
 func idDigit(ch byte) bool {
 	return '0' <= ch && ch <= '9'
+}
+
+func (l *Lexer) peekChar() byte {
+	if l.readPosition >= len(l.input) {
+		return 0
+	} else {
+		return l.input[l.readPosition]
+	}
 }
